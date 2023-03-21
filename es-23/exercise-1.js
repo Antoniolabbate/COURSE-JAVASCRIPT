76b61e560280f828b3a20da872bdff80a2119cff
Antonio L'Abbate <antolabb06@gmail.com>
2023-03-21 15:58:02 +0100

    es 9

--- es-23/exercise-1.js
+++ es-23/exercise-1.js
@@ -1,10 +1,7 @@
 const person = {
   firstName: "Mario",
   lastName: "Rossi",
-  age: 25,
-}
-const keys = (Object.keys(person));
-console.log(keys);
-console.log(person);
+  age: 25
+};
 
-// Print values of person using Object.keys
+console.log(Object.keys(person).map(key => `${key}: ${person[key]}`).join(", "));
