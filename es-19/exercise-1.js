ed8586e7e60d02ba25effcc8ea708051c4e0a9be
Antonio L'Abbate <antolabb06@gmail.com>
2023-03-21 15:46:12 +0100

    es 5

--- es-19/exercise-1.js
+++ es-19/exercise-1.js
@@ -3,10 +3,14 @@ const person1 = {
   lastName: "Doe",
   age: 25,
 };
-person1.firstName = "Simon";
+
 const person2 = person1;
 
-// Facendo in questo modo ho modificato la prima constante aggiungendo "Simon", senza andare a creare un altro oggetto.
+person2.firstName = "Simon"
+
+// Modify the property firstName of the person2 in "Simon".
 
 console.log(person1);
 console.log(person2);
+
+// indicando person2 = person1 si effettua copia by reference dell'oggetto per questo modificando il value firstName di person2 viene sovrascritto anche per person1.
