f410659794ef7e47b270535ae6bcbe3e366af976
antonio l'abbate <antolabb06@gmail.com>
2023-04-11 17:38:03 +0200

    exercise 34

---
+++ es-73/exercise.js
@@ -0,0 +1,12 @@
+function  printAsyncName(callback,str){
+    setTimeout(()=>{
+        callback()
+    },1000);
+    setTimeout(()=>{
+        console.log(str)
+    },2000);
+}
+function hello(){
+    console.log("Hello")
+}
+printAsyncName(hello, "antonio")
